fa4ce04463af5c97cd53a0f49726298a556a9cb9
Kirill Tkachev <tkachev89@mail.ru>
2026-08-04 20:08:08 +0300

    Исправил инициализацию базы данных для тестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
 import pytest
 from sqlmodel import Session, delete
 
-from database import engine
+from database import engine, create_db_and_tables
 from models import Link
 
 
 @pytest.fixture(autouse=True)
 def clear_database():
+    create_db_and_tables()
+
     with Session(engine) as session:
         session.exec(delete(Link))
         session.commit()
